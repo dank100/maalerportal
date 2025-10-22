d959e9e88a01ba0d140cb83c1677eb11d7982a4d
Dan Kristiansen <dkr@centrica.dk>
2025-10-22 08:22:58 +0200

    Fix late_reading calculation to remove timezone information

--- custom_components/maalerportal/sensor.py
+++ custom_components/maalerportal/sensor.py
@@ -160,7 +160,7 @@ class MaalerportalStatisticSensor(SensorEntity):
         value = 0
         if newest_reading is not None:
             value = newest_reading.value
-            late_reading = newest_reading.timestamp.astimezone(timezone.utc)
+            late_reading = newest_reading.timestamp.replace(tzinfo=None)
             if start_time < late_reading:
                 start_time = late_reading
 
