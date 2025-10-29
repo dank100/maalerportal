1450c166a277a1c6f87aeb22b0aa3d5722e5c072
Dan Kristiansen <15105728+dank100@users.noreply.github.com>
2025-10-29 19:24:55 +0100

    Comment out reading update logic
    Comment out code that updates native value and state.

--- custom_components/maalerportal/sensor.py
+++ custom_components/maalerportal/sensor.py
@@ -196,9 +196,9 @@ class MaalerportalStatisticSensor(SensorEntity):
             _LOGGER.debug("Adding %s readings for %s", len(statistics), self.entity_id)
             async_import_statistics(self.hass, metadata, statistics)
 
-            if newest_reading is not None:
-                self._attr_native_value = float(newest_reading.value)
-                self.schedule_update_ha_state()
+            #if newest_reading is not None:
+               # self._attr_native_value = float(newest_reading.value)
+                #self.schedule_update_ha_state()
         else:
             _LOGGER.debug("No new readings found")
 
